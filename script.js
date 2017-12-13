114cfb71b3b7afb18542b648b6492dfc1105eeef
AWSANIMA <pieteroehlers@gmail.com>
2017-12-13 18:11:04 +0100

    index.html

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ window.onload = function() {
     var c=document.getElementById("myCanvas");
     var ctx=c.getContext("2d");
     var img=document.getElementById("spaceCraft");
+    var img2=document.getElementById("meteor");
     ctx.drawImage(img,10,10);
+    ctx.drawImage(img2,10,10);
 }
